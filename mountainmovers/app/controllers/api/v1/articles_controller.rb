80fbd87e6fbaabe5d83e913ed61666f0e125e167
Hannah Aurand <aurand.hannah@gmail.com>
2018-07-31 14:17:52 -0400

    Add destroy and update to controller

--- mountainmovers/app/controllers/api/v1/articles_controller.rb
+++ mountainmovers/app/controllers/api/v1/articles_controller.rb
@@ -5,6 +5,41 @@ module Api
                 articles = Article.order('created_at DESC');
                 render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
             end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded article', data:article}, status: :ok
+            end
+
+            def create 
+                article = Article.new(article_params)
+                if article.save
+                    render json: {status: 'SUCCESS', message: 'Saved Article', data:article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Article Not Saved', data:article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {status: 'SUCCESS', message: 'DELETED ARTICLE SUCCESSFULLY', data:article}, status: :ok
+            end
+
+            def update
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params)
+                    render json: {status: 'SUCCESS', message: 'UPDATED ARTICLE', data:article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'ARTICLE NOT UPDATED', data:article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def article_params 
+                params.permit(:title, :body)
+            end
         end
     end
 end
